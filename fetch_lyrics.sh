78a8747bc2a0e3ebbde51b5d61e27f69b692fbb2
maxned <maxned@yahoo.com>
2021-11-06 12:04:11 -0700

    Update fetch_lyrics to work in the home dir

--- fetch_lyrics.sh
+++ fetch_lyrics.sh
@@ -3,8 +3,8 @@ SERVICE='ProPresenter'
 if pgrep -xq -- "${SERVICE}"; then
   osascript -e 'display alert "Error: ProPresenter is open!" message "Close ProPresenter before fetching lyrics."'
 else
-  eGFC_PATH=$HOME/Documents/ProPresenter/Libraries/eGFC
-  rGFC_PATH=$HOME/Documents/ProPresenter/Libraries/rGFC
+  eGFC_PATH=$HOME/ProPresenter/Libraries/eGFC
+  rGFC_PATH=$HOME/ProPresenter/Libraries/rGFC
 
   if [ ! -d $eGFC_PATH ]; then
     mkdir $eGFC_PATH
