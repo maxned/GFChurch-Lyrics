7a14df6ece3b90bc423e1475c08dbc7552c88c94
Jacob Voronin <jacob.voronin@gmail.com>
2025-05-16 21:07:11 -0700

    Update fetch_lyrics.sh

--- fetch_lyrics.sh
+++ fetch_lyrics.sh
@@ -5,6 +5,7 @@ if pgrep -xq -- "${SERVICE}"; then
 else
   eGFC_PATH=$HOME/ProPresenter/Libraries/eGFC
   rGFC_PATH=$HOME/ProPresenter/Libraries/rGFC
+  choir_PATH=$HOME/ProPresenter/Libraries/choir
 
   if [ ! -d $eGFC_PATH ]; then
     mkdir $eGFC_PATH
@@ -13,14 +14,20 @@ else
   if [ ! -d $rGFC_PATH ]; then
     mkdir $rGFC_PATH
   fi
+  
+  if [ ! -d $choir_PATH ]; then
+    mkdir $choir_PATH
+  fi
 
   git clone https://github.com/maxned/GFChurch-Lyrics.git
 
   rm -rf $eGFC_PATH/*
   rm -rf $rGFC_PATH/*
+  rm -rf $choir_PATH/*
 
   mv GFChurch-Lyrics/eGFC/* $eGFC_PATH/
   mv GFChurch-Lyrics/rGFC/* $rGFC_PATH/
+  mv GFChurch-Lyrics/choir/* $choir_PATH/
 
   rm -rf GFChurch-Lyrics
 fi
